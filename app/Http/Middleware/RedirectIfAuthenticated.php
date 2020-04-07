0e479a29fdb34cbe06c6526c76eb7e2457da15a4
Pooja Yadav <poojayadav@aquadsoft.com>
2020-04-07 14:44:23 +0530

    worked on datatype of conatct number of user

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,10 +18,12 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+         if (Auth::guard($guard)->check()) {
+            return redirect('/home');
         }
-
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect('/admin');
+        }
         return $next($request);
     }
 }
