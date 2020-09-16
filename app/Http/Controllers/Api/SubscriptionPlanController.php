56634a5807ff3f776757d298cb44dda2f0416c69
Ubuntu <ubuntu@ip-172-31-46-57.us-east-2.compute.internal>
2020-09-16 17:26:06 +0000

    worked on api of subscription

--- app/Http/Controllers/Api/SubscriptionPlanController.php
+++ app/Http/Controllers/Api/SubscriptionPlanController.php
@@ -18,27 +18,20 @@ class SubscriptionPlanController extends Controller
 
 
 
-   public function getSubscriptionPlan(Request $request){
+  public function getSubscriptionPlan(Request $request){
+   
+   
+   
    $video_id = $request->video_id;
    $status = false;
    $data = [];
-   $subscription_details = UserSubscriptionDetails::where('user_id',$request->user_id)->first();
-   $videosubscriptioncategories_id=[];
-  
-
+   $subscription_detail = UserSubscriptionDetails::where('user_id',$request->user_id)->get();
    $subscribedvideo_id = PremiumVideos::where('id',$request->video_id)->with(['subscriptionplandetails.subscriptioncategory','subscriptionplandetails.subscriptionplan','subscriptionplandetails.additionalbenifits'])->first();
-   
-   
-   
-   
-   
   
-     
-     
- 
-if($subscribedvideo_id->subscriptionplandetails->count() == null){
+   // Admin has not added any subscription plan for the video
+    if($subscribedvideo_id->subscriptionplandetails->count() == null){
 
-$status = false;
+    $status = false;
     $data = [
       'status' => $status,
       
@@ -48,21 +41,27 @@ $status = false;
     return Response::json(['code' => 200,'status' => true, 'message' => 'Admin has not added any data for this video','data'=>$data]);
    
    }
-     
-     
-
- 
    
+   
+   
+   // if user is subscribed
+  
+   if($subscription_detail !=  null)
+  {
 
+
+  //check every subscriptions details
+
+
+  foreach($subscription_detail as $key => $subscription_details){
+  
  
- 
- if($subscription_details != null){
- 
-    if($subscription_details->product == 'Subscription Plan'){
-    
-      $videoAccessDetails = UserSubscribedVideosDetails::where('premium_video_id',$video_id)->where('user_subscription_id',$subscription_details->id)->with(['usersubscriptiondetails.usersubscriptionplandetails'])->with('premiumvideo')->first();
-      
+  // check video access details with user subscription id
+   $videoAccessDetails = UserSubscribedVideosDetails::where('premium_video_id',$video_id)->where('user_subscription_id',$subscription_details->id)->with(['usersubscriptiondetails'])->with('premiumvideo')->first();
+  
+    // check whether it is accessible or not 
       if($videoAccessDetails != null){
+     
       if($videoAccessDetails->access == true){
       $status = true;
       $data = [
@@ -75,77 +74,57 @@ $status = false;
       return Response::json(['code' => 200,'status' => true, 'message' => 'User is Valid for the video','data'=> $data]);
       
       }
-      }
-      else{
-      $status =false;
-     $data = [
+  
+  
+  
+  }
+
+
+} 
+
+     $status =false;
+       $data = [
       'status' => $status,
       
       'details' => $subscribedvideo_id,
       
       ];
       return Response::json(['code' => 200,'status' => true, 'message' => 'User is not valid For the Video','data'=> $data]);
-      }
-      
-    }
-    
-    
-    
-    if($subscription_details->product == 'Video'){
-     
-     
-     $videoAccessDetails = UserSubscribedVideosDetails::where('premium_video_id',$video_id)->where('user_subscription_id',$subscription_details->id)->with([   'usersubscriptiondetails'])->with('premiumvideo')->first();
-      if($videoAccessDetails != null){
-      if($videoAccessDetails->access == true){
-      $status = true;
-      $data = [
-      'status' => $status,
-      
-      'details' => $subscribedvideo_id,
-      
-      ];
-      return Response::json(['code' => 200,'status' => true, 'message' => 'User is Valid for the video','data'=>$data]);
-      
-      }
-      
-      }
-      
-      else{
-      
+
+  
+}
+
+
+    else{
       $status =false;
-     $data = [
+       $data = [
       'status' => $status,
       
       'details' => $subscribedvideo_id,
       
       ];
-      return Response::json(['code' => 200,'status' => true, 'message' => 'User is not valid For the Video','data'=>$data]);
-      }
+      return Response::json(['code' => 200,'status' => true, 'message' => 'User is not valid For the Video','data'=> $data]);
+      }   
+   
+   
+   
+  
+
+
+
+
+
+
+      
     
-    }
- 
- }
- 
- 
- 
- else{
- 
- 
- 
- 
-  $status =false;
-      $data = [
-      'status' => $status,
       
-      'details' => $subscribedvideo_id,
-      
-      ];
-      return Response::json(['code' => 200,'status' => true, 'message' => 'User is not valid For the Video','data'=>$data]);
  
- 
- }
- 
- 
+   
+   
+   
+   
+   
+  
 
 
 
