7fd837c5925fe547c9f6be971ffe95ecd1ddbf7d
Pooja Yadav <poojayadav@aquadsoft.com>
2021-01-11 09:38:26 +0000

    UserController.php edited online with Bitbucket

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\Model\SubscriptionCategory;
 use App\Model\SubscriptionPlan;
+use App\Model\UserSubscribedVideosDetails;
 use Carbon\Carbon;
 use Response;
+use DB;
 class UserController extends Controller
 {
 
@@ -38,8 +40,26 @@ class UserController extends Controller
      return back()->with('status',100)->with('type','success')->with('message','User deleted Successfully');
 
    }
-    public function getAllUserDetail(Request $request)
+  public function getAllUserDetail(Request $request)
    {
-       echo "Devlopment is going On";
+       
+       $page = 'subscribed premium videos';
+       $userId=$request->id;
+       $usersubscriptiondetailsId=$request->usersubscriptiondetailsId;
+       $premium_videos = DB::select("select premium_videos.name,user_subscribed_videos_details.id,user_subscribed_videos_details.access FROM user_subscribed_videos_details INNER JOIN premium_videos ON user_subscribed_videos_details.premium_video_id = premium_videos.id where user_subscription_id='$usersubscriptiondetailsId'");
+       return view('admin.subscribed-premium_videos',compact('page','premium_videos'));
+       
    }
+  public function changedPremiumVideoAccess(Request $request)
+   {
+       $videoid=$request->videoid;
+       $status=$request->status;
+       $updateData=array(
+           "access"=>$status
+           );
+       UserSubscribedVideosDetails::where('id',$videoid)->update($updateData);
+       return back()->with('status',100)->with('type','success')->with('message','Status Has been changed');
+       
+   }
+   
 }
