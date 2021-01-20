efcc023bcbf67de3c7276cbf3cd3727537f72523
Pooja Yadav <poojayadav@aquadsoft.com>
2021-01-20 13:37:44 +0000

    all-users.blade.php edited online with Bitbucket

--- resources/views/admin/all-users.blade.php
+++ resources/views/admin/all-users.blade.php
@@ -126,5 +126,12 @@
             </div>
         </div>
     </section>
+     <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+    <script>
+    $(function(){
+    $("#all-user-datatable").dataTable();
+  })
+  </script>
 
 @endsection
